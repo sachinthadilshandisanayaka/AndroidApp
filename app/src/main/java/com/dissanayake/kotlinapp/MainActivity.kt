187323d89ac1a6f922d772f32608cbc9b270de12
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-10-31 22:54:42 +0530

    updated

--- app/src/main/java/com/dissanayake/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/dissanayake/kotlinapp/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.dissanayake.kotlinapp
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +16,27 @@ class MainActivity : AppCompatActivity(){
         // R is predefine class
         // attach layout to the main Activity
         setContentView(R.layout.activity_main)
+
+        futureButton.setOnClickListener {
+            // code
+            // we can see this msg in cmd as MainActivity child msg
+            Log.i("MainActivity", "Future is near")
+
+            // show message in the screen
+
+            Toast.makeText(this, "Future is near", Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
