f90424726ebffb49006a03b5a4bf95636b21cd90
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-11-03 22:56:48 +0530

    fixed errors

--- app/src/main/java/com/dissanayake/kotlinapp/HobbiesActivity.kt
+++ app/src/main/java/com/dissanayake/kotlinapp/HobbiesActivity.kt
@@ -2,10 +2,19 @@ package com.dissanayake.kotlinapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_hobbies.*
 
 class HobbiesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hobbies)
+
+        val layoutManager = LinearLayoutManager(this)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        recycleView.layoutManager = layoutManager
+
+        val adapter = HobbyAdapter(this, Supplier.hobbies)
+        recycleView.adapter = adapter
     }
 }
