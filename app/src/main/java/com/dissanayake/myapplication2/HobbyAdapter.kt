4531c8f4db11c099a6de80813dd2e0a79feb9768
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-11-06 11:33:03 +0530

    updated

--- app/src/main/java/com/dissanayake/myapplication2/HobbyAdapter.kt
+++ app/src/main/java/com/dissanayake/myapplication2/HobbyAdapter.kt
@@ -1,10 +1,13 @@
 package com.dissanayake.myapplication2
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.list_item.view.*
 
 
@@ -24,8 +27,42 @@ class HobbyAdapter(val context: Context, val hobbies : List<Hobby>): RecyclerVie
     }
     // one item is a view holder
     inner class MyViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+
+        var currentPosition: Int = 0
+        var currentHobby: Hobby? = null
+
+        init {
+            itemView.setOnClickListener {
+                Toast.makeText(context, currentHobby!!.title+ " Clicked !", Toast.LENGTH_SHORT).show()
+            }
+            itemView.imgShare.setOnClickListener {
+                val message : String = "My hobby is : " + currentHobby!!.title
+                val intent = Intent()
+                intent.action = Intent.ACTION_SEND
+                intent.putExtra(Intent.EXTRA_TEXT, message)
+                intent.type = "text/plain"
+                context.startActivity(Intent.createChooser(intent, "Share to :"))
+            }
+        }
+
         fun setData(hobby : Hobby, pos: Int) {
             itemView.textTitle.text = hobby!!.title
+            this.currentHobby = hobby
+            this.currentPosition = pos
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
