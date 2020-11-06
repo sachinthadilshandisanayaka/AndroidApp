dccfe0db3d55dc94259e8ba2641d66cc4cde714f
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-11-07 01:20:28 +0530

    add extention to Toast method

--- app/src/main/java/com/dissanayake/myapplication2/Extentions.kt
+++ app/src/main/java/com/dissanayake/myapplication2/Extentions.kt
@@ -1,9 +1,8 @@
 package com.dissanayake.myapplication2
 
-import android.app.Activity
 import android.content.Context
 import android.widget.Toast
 
-fun Context.showToast(message :String) {
-    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+fun Context.showToast(message :String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, message, duration).show()
 }
